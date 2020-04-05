412c2da2899127ec41d6342bb565ec4a2c544618
Kiril Chobanov <62597135+kiril2000@users.noreply.github.com>
2020-04-05 15:21:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CheckNumberIsEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string evenNumber = "The number is even";
+            string oddNumber = "The number is odd";
+
+            string result = number % 2 == 0 ? evenNumber : oddNumber;
+
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine(evenNumber);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(oddNumber);
+            //}     
+
+            Console.WriteLine(result);
+        }
+    }
+}
